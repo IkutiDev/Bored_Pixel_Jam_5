d319cf7dc1ff44c087c3714e9799f95c0cb9fcc4
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-15 12:02:26 +0200

    Character movement, camera following player, simple demo level

--- Cyberpunk Game/Assets/Demo Assets/Scripts/PlatformController2D.cs
+++ Cyberpunk Game/Assets/Demo Assets/Scripts/PlatformController2D.cs
@@ -1,8 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformController2D : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed=3f;
+
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 movementVector2 = Vector2.zero;
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movementVector2 = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.MovePosition(_rigidbody2D.position + speed * Time.fixedDeltaTime * movementVector2);
+    }
 }
