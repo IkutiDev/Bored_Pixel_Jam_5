06396aecad24aae6f91d2deea1c0d73e9bf8358e
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-17 00:57:11 +0200

    Fans work good enough now

---
+++ Cyberpunk Game/Assets/Demo Assets/Scripts/BlowAway.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowAway : MonoBehaviour
+{
+    enum Direction
+    {
+        Up,
+        Down,
+        Right,
+        Left,
+    }
+
+    [SerializeField] private Direction direction = Direction.Left;
+    [SerializeField] private float speedOfAirForce=15000f;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Object entetered fan air area");
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        switch (direction)
+        {
+            case Direction.Right:
+                other.attachedRigidbody.AddForce(speedOfAirForce * Time.deltaTime * -Vector2.left);
+                break;
+            case Direction.Left:
+                other.attachedRigidbody.AddForce(speedOfAirForce * Time.deltaTime * -Vector2.right);
+                break;
+            case Direction.Down:
+                other.attachedRigidbody.AddForce(speedOfAirForce * Time.deltaTime * -Vector2.up);
+                break;
+            case Direction.Up:
+                other.attachedRigidbody.AddForce(speedOfAirForce * Time.deltaTime * -Vector2.down);
+                break;
+            default:
+                other.attachedRigidbody.AddForce(speedOfAirForce * Time.deltaTime * -Vector2.right);
+                break;
+        }
+        
+        //Vector2 position = transform.position;
+        //Vector2 targetPosition = other.transform.position;
+        //Vector2 direction = targetPosition - position;
+        //direction.Normalize();
+        //targetPosition += speedOfAirForce * Time.deltaTime * direction;
+        //other.transform.position = targetPosition;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Left the fan air area");
+    }
+}
--- Cyberpunk Game/Assets/Demo Assets/Scripts/Fan.cs
+++ Cyberpunk Game/Assets/Demo Assets/Scripts/Fan.cs
@@ -21,6 +21,7 @@ public class Fan : MonoBehaviour
         {
             Debug.Log(gameObject.name+" turned off.");
             fanToggle = false;
+            gameObject.transform.GetChild(0).gameObject.SetActive(false); // If Windarea is not first child change this line !!!
             StartCoroutine(nameof(TurnOn));
         }
     }
@@ -29,6 +30,7 @@ public class Fan : MonoBehaviour
     {
         yield return new WaitForSeconds(offTime);
         Debug.Log(gameObject.name + " turned on.");
+        gameObject.transform.GetChild(0).gameObject.SetActive(true); // If Windarea is not first child change this line !!!
         fanToggle = true;
     }
 }
