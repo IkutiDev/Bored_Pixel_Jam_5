083e00ebf52c10dc240b1cb648d0bf01c3b57f84
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-17 22:02:58 +0200

    Camera transition is operational

---
+++ Cyberpunk Game/Assets/Demo Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    private CinemachineVirtualCamera CVC;
+    private void Awake()
+    {
+        CVC = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (CVC.Priority == 0)
+            {
+                CVC.Priority = 2;
+            }
+            else
+            {
+                CVC.Priority = 0;
+            }
+        }
+    }
+}
