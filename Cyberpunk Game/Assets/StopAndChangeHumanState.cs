560ac54611c655becaa3b54d97b7562086d22ade
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-19 15:22:32 +0200

    Human State management and triggers

---
+++ Cyberpunk Game/Assets/StopAndChangeHumanState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAndChangeHumanState : MonoBehaviour
+{
+    [SerializeField] private ControlHuman.HumanState newState;
+    [SerializeField] private float timeToWait;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject human=collision.gameObject;
+        if (collision.CompareTag("Human"))
+        {
+            StartCoroutine(ChangeState(timeToWait,human));
+        }
+    }
+
+    private IEnumerator ChangeState(float time,GameObject human)
+    {
+        human.GetComponent<ControlHuman>().currentState = ControlHuman.HumanState.Stop;
+        yield return new WaitForSeconds(time);
+        human.GetComponent<ControlHuman>().currentState = newState;
+
+    }
+}
