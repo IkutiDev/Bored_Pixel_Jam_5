11406517efebe7d161430b7a47bc7d045c73ed86
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-21 10:20:48 +0200

    Added music and music manager

---
+++ Cyberpunk Game/Assets/MusicPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private float songDelay=5f;
+    private AudioSource audioSource;
+    private float timer;
+    private int currentMusicClip=0;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audioClips[currentMusicClip];
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= audioSource.clip.length+songDelay)
+        {
+            timer = 0;
+            currentMusicClip++;
+            if (currentMusicClip >= audioClips.Length)
+            {
+                currentMusicClip = 0;
+            }
+            audioSource.clip = audioClips[currentMusicClip];
+            audioSource.Play();
+        }
+    }
+}
