840c2c166467484f3786e85b0ae9a34547b3c19d
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-21 16:58:00 +0200

    DeathTrigger added to the game

---
+++ Cyberpunk Game/Assets/Kill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Human"))
+        {
+            GameObject.Find("GameStateManager").GetComponent<GameStateManager>().LooseGame();
+        }
+    }
+}
---
+++ Cyberpunk Game/Assets/UpAndDown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpAndDown : MonoBehaviour
+{
+    public bool goDown;
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= 0f)
+        {
+            goDown = false;
+            
+        }
+
+        if (transform.position.y >= 2f)
+        {
+            goDown = true;
+        }
+
+        if (goDown==false)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.01f);
+        }
+
+        if (goDown)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + 0.01f);
+        }
+    }
+}
